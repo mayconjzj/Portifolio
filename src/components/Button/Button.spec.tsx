7b509e7756737bd6b10555b11bd25f864dce9d0d
mayconxrz@gmail.com <mayconxrz@gmail.com>
2023-09-27 11:10:19 -0400

    new commit

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -4,7 +4,7 @@ import { Button } from './Button';
 
 describe('Button', () => {
   it('should render the button', () => {
-    const { asFragment } = render(<Button>TESt</Button>);
+    const { asFragment } = render(<Button>TESTE</Button>);
     expect(asFragment()).toBeTruthy();
 
     expect(asFragment()).toMatchSnapshot();
