791ca9b76b34ca93b505b66810dcef0d4b61038d
mayconxrz@gmail.com <mayconxrz@gmail.com>
2024-01-23 12:19:37 -0400

    Ttile Corrigido

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -7,14 +7,13 @@ export const Title = ({ children, ...rest }: TitleProps) => {
     <>
       <h1
         className={twMerge(
-          'text-3xl font-bold relative z-10 after:content-[] after:absolute after:bottom-[-6px] after:left-0 after:w-52 after:h-[2px] after:bg-gradient-to-r after:from-blue-500 after:to-black',
+          'text-3xl font-bold relative z-10 after:content-[""] after:absolute after:bottom-[-6px] after:left-0 after:w-52 after:h-[2px] after:bg-gradient-to-r after:from-blue-500 after:to-black',
           rest.className
         )}
         {...rest}
       >
         {children}
       </h1>
-      ;
     </>
   );
 };
